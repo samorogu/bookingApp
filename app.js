15d4487614da631aa55861574885e9b209e5a35e
™samorogu™ <samorogu@gmail.com>
2019-05-05 14:40:02 -0500

    adding mongodb to persist the events data

--- app.js
+++ app.js
@@ -77,11 +77,17 @@ app.use(
   })
 );
 
-let uri =
-  "mongodb://bookingUser:<Password>@cluster0-shard-00-00-ly89x.mongodb.net:27017,cluster0-shard-00-01-ly89x.mongodb.net:27017,cluster0-shard-00-02-ly89x.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+//let uri ="mongodb://bookingUser:<Password>@cluster0-shard-00-00-ly89x.mongodb.net:27017,cluster0-shard-00-01-ly89x.mongodb.net:27017,cluster0-shard-00-02-ly89x.mongodb.net:27017/events-react-dev?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 
 mongoose
-  .connect(uri)
+  .connect(
+    `"mongodb://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@cluster0-shard-00-00-ly89x.mongodb.net:27017,cluster0-shard-00-01-ly89x.mongodb.net:27017,cluster0-shard-00-02-ly89x.mongodb.net:27017/${
+      process.env.MONGO_DB
+    }?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"`,
+    { useNewUrlParser: true }
+  )
   .then(() => {
     app.listen(3000);
   })
